f2e6aa9e8c64ccea8445ff9f0547d9ec6a4f35ed
akdubey99 <andubey@isystango.com>
2021-09-06 11:04:17 +0530

    Updated url files of project and app

--- boilerplate/urls.py
+++ boilerplate/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url, include
-from django.urls import path
+from django.urls import path,include
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url(r'^api/v1.0/boilerplate_apps/',
+    path('admin/', admin.site.urls),
+    path('api/v1.0/boilerplate_apps/',
         include("boilerplate_app.urls", namespace="boilerplate_app-api")),
-    path("graphql/", GraphQLView.as_view(graphiql=True))
+    path("graphql/", GraphQLView.as_view(graphiql=True)),
 ]
--- boilerplate_app/urls.py
+++ boilerplate_app/urls.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 import logging
 
 # Django imports.
-from django.conf.urls import url
+from django.urls import path,include
 
 # Rest Framework imports.
 
@@ -22,14 +22,17 @@ from boilerplate_app.views import (
                           )
 from boilerplate_app.swagger import schema_view
 
+
+
+
 app_name = 'boilerplate_app'
 
 urlpatterns = [
-    url(r'^test/$', TestAppAPIView.as_view(), name='boilerplate_app'),
-    url(r'^register/$', RegistrationAPIView.as_view(), name='register-api'),
-    url(r'^login/$', LoginView.as_view(), name='login-api'),
-    url(r'^logout/$', LogoutView.as_view(), name='logout-api'),
-    url(r'^list/users/$', UserAPIView.as_view(), name='user-api'),
-    url(r'^project/$', ProjectAPIView.as_view(), name='project-api'),
-    url(r'^docs/$', schema_view, name="schema_view"),
+    path('test/', TestAppAPIView.as_view(), name='boilerplate_app'),
+    path('register/', RegistrationAPIView.as_view(), name='register-api'),
+    path('login/', LoginView.as_view(), name='login-api'),
+    path('logout/', LogoutView.as_view(), name='logout-api'),
+    path('list/users/', UserAPIView.as_view(), name='user-api'),
+    path('project/', ProjectAPIView.as_view(), name='project-api'),
+    path('docs/', schema_view.with_ui("swagger", cache_timeout=0), name="schema_view"),
 ]
