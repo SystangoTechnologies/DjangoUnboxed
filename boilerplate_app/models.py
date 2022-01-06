de608f5ea8dd9f685be0c60401c590a8f2231845
taher-systango <tmalubhaiwala@isystango.com>
2022-01-06 17:55:48 +0530

    Fix docstring

--- boilerplate_app/models.py
+++ boilerplate_app/models.py
@@ -64,16 +64,16 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name_plural = _('users')
 
     def get_full_name(self):
-        '''
+        """
         Returns the first_name plus the last_name, with a space in between.
-        '''
+        """
         full_name = '%s %s' % (self.first_name, self.last_name)
         return full_name.strip()
 
     def get_short_name(self):
-        '''
+        """
         Returns the short name for the user.
-        '''
+        """
         return self.first_name
 
     def __str__(self):
