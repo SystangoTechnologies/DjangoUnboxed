79bcaf59e90abe740bcccb36e05c4123116dd31d
taher-systango <tmalubhaiwala@isystango.com>
2022-01-06 17:55:24 +0530

    Enable static file serving

--- boilerplate/urls.py
+++ boilerplate/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1.0/boilerplate_apps/',
-        include("boilerplate_app.urls", namespace="boilerplate_app-api")),
-    path("graphql/", GraphQLView.as_view(graphiql=True)),
-]
+                  path('admin/', admin.site.urls),
+                  path('api/v1.0/boilerplate_apps/',
+                       include("boilerplate_app.urls", namespace="boilerplate_app-api")),
+                  path("graphql/", GraphQLView.as_view(graphiql=True)),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
