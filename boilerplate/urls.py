6b8faeed7d5890e493b1cc4371ae8b291738f994
Abhay Raghuvanshi <araghuvanshi@isystango.com>
2022-02-01 22:44:28 +0530

    Minor fixes and some refactoring

--- boilerplate/urls.py
+++ boilerplate/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
+from rest_framework import urls
 
 from graphene_django.views import GraphQLView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('boilerplate_apps/', include("boilerplate_app.urls")),
+    path('auth/', include(urls)),
     path('api/', include("rest.urls")),
     path("graphql/", GraphQLView.as_view(graphiql=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
